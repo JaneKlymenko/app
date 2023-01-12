a1c981b42d5a72f3a16fefabc58c39492f6f3354
Yana <yklymenko@lacosolutions.net>
2023-01-12 18:44:43 +0200

    add some files in component

---
+++ src/components/countersList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Counter from "./counter";
+
+const CountersList = () =>{
+    const counters = [{id:0, value:0}, {id:1, value:6}, {id:2,value:0}];
+    return (<>
+        {counters.map((count)=>(
+            <Counter key={count.id}
+                value = {count.value}
+            />
+            
+        ))}
+    
+    </>);
+}
+
+export default CountersList;
