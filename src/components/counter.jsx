a3e5e5eea30e5a41061c5e4cfaccfabd9b2737fb
Yana <yklymenko@lacosolutions.net>
2023-01-16 11:34:36 +0200

    new functions added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 const Counter=(props)=> {
-    console.log(props);
-    const [value, setValue] = useState(props.value);
+    const {value} = props;
 
     const formatValue = () => {
         return value===0? 'empty' :value;
@@ -13,15 +12,15 @@ const Counter=(props)=> {
     };
 
     const handleIncrement=()=>{
-        setValue( (prevState) => prevState + 1);
+       props.onIncrement(props.id);
     };
     const handleDecrement=()=>{
-        setValue( (prevState) => prevState - 1);
+        props.onDecrement(props.id);
     };
 
     return (
         <div>
-        
+            <span>{props.name}</span>
             <span className={getBadgeClasses()}>{formatValue()}</span>
             <button 
                 className='btn btn-primary btn-sm m-2' 
@@ -35,6 +34,8 @@ const Counter=(props)=> {
             >
                 -
             </button>
+            <button className="btn btn-danger btn-sm m-2" 
+            onClick={()=>props.onDelete(props.id)}>Delete</button>
         </div>
     );
 };
